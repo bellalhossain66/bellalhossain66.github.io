739a99c8717daccf30a533f6a9abffc991d6794c
bellalhossain66 <bellalhoss66@gmail.com>
2025-03-07 16:42:00 +0600

    experience update

--- assets/js/indexes.js
+++ assets/js/indexes.js
@@ -8,3 +8,25 @@ $(document).on('click', '.skill-item', function(event) {
     $('.tab-pane').removeClass('active')
     $(id).addClass('active')
 })
+
+$(document).ready(function(){
+    const start = new Date("2022-10-01");
+    const start2 = new Date("2025-03-01");
+    const now = new Date();
+    let years = now.getFullYear() - start.getFullYear();
+    let months = now.getMonth() - start.getMonth();
+    if (months < 0) {
+        years--;
+        months += 13;
+    }
+    $('#job-duration').text(years+'yr '+months+'mo');
+    years = now.getFullYear() - start2.getFullYear();
+    months = now.getMonth() - start2.getMonth();
+    if (months < 0) {
+        years--;
+        months += 13;
+    }else{
+        months = 1;
+    }
+    $('#job-duration2').text(years>0?(years+'yr '):''+months+'mo');
+});
